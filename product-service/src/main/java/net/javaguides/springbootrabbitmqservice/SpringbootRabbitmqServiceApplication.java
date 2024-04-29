ef17b1250d49012aff9b5b0f95cd0c320cbf42fd
deepthi <96069678+deepthisomayaji@users.noreply.github.com>
2024-04-29 15:07:21 +0530

    Add files via upload

---
+++ product-service/src/main/java/net/javaguides/springbootrabbitmqservice/SpringbootRabbitmqServiceApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.springbootrabbitmqservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRabbitmqServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRabbitmqServiceApplication.class, args);
+	}
+
+}
---
+++ product-service/src/main/java/net/javaguides/springbootrabbitmqservice/dto/User.java
@@ -0,0 +1,8 @@
+package net.javaguides.springbootrabbitmqservice.dto;
+import lombok.Data;
+@Data
+public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+}
